805c879355a0986fee1a3b78f07df8d0d3409a05
omxrm <oamorales@uncg.edu>
2025-10-10 12:27:25 -0400

    Copy all the contents of the SprintBoot project into my cloned repository

---
+++ src/main/java/com/csc340/cats_crud_api/CatsCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.cats_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc340/cats_crud_api/CatsCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.cats_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
